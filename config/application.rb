acfb0197c7a32e680ac62f3731c2622605537840
saitou1128 <takuma.k419575@gmail.com>
2026-03-27 22:48:34 +0900

    Applicationfilechange4

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails"
+
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -14,5 +15,7 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     config.load_defaults 7.2
+
+    config.assets.paths << Rails.root.join("app/assets")
   end
 end
